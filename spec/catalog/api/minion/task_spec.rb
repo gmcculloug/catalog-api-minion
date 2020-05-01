7992b6c47536a1abaa8302c9fc7cd273a80ff795
Greg McCullough <gmccullo@redhat.com>
2020-05-01 09:25:08 -0400

    Merge pull request #34 from eclarizio/task_id_handling
    Use 'id' key to fetch the task id from within a kafka task payload

--- spec/catalog/api/minion/task_spec.rb
+++ spec/catalog/api/minion/task_spec.rb
@@ -9,7 +9,7 @@ describe Catalog::Api::Minion::Task do
 
     let(:message) { ManageIQ::Messaging::ReceivedMessage.new(nil, "Task.update", payload, headers, nil, nil) }
 
-    let(:payload) { {"task_id" => "123"} }
+    let(:payload) { {"id" => "123"} }
     let(:payload_params) do
       {:payload => message.payload, :message => message.message}
     end
@@ -20,12 +20,8 @@ describe Catalog::Api::Minion::Task do
       end
     end
 
-    before do
-      stub_request(:post, "http://localhost:3000/internal/v1.0/notify/task/123")
-    end
-
     context "when there is no error" do
-      it "posts a payload to the order item endpoint" do
+      it "posts a payload to the order item endpoint with a task id" do
         task_minion.perform(message)
         expect(a_request(:post, "http://localhost:3000/internal/v1.0/notify/task/123").with(
           :body    => payload_params,
